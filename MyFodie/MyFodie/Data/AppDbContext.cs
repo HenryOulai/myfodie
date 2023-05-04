ced2584a818b0cd668c45b9301caecaef6c4ceac
flyassburitto <112472563+flyassburitto@users.noreply.github.com>
2023-05-04 10:41:39 +0200

    new

---
+++ MyFodie/MyFodie/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyFodie.Models;
+using System.Collections.Generic;
+
+namespace MyFodie.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Account> Accounts { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
