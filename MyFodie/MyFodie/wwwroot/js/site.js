96d5b0332a9b0a6767793b4c8dd1a623cf5be8b1
flyassburitto <henry_oulai@hotmail.com>
2023-05-16 07:29:16 +0200

    changes

--- MyFodie/MyFodie/wwwroot/js/site.js
+++ MyFodie/MyFodie/wwwroot/js/site.js
@@ -1,17 +1,22 @@
-﻿var slideIndex = 1;
-showDivs(slideIndex);
+﻿// Slideshow functionality
+let slideIndex = 1;
 
 function plusDivs(n) {
     showDivs(slideIndex += n);
 }
 
 function showDivs(n) {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    if (n > x.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = x.length }
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
+    const slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) {
+        slideIndex = 1;
     }
-    x[slideIndex - 1].style.display = "block";
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
 }
+
+showDivs(slideIndex);
